7ae48d0796ac926874d51fc9e61d408e3f01f483
Vitor Cyriaco <vitorcyriaco@outlook.com>
2025-03-18 21:35:26 -0300

    Refractor: turn Main into Home
    init JavaFx in this class

---
+++ src/view/Home.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Home extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
