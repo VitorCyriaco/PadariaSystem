71ec35694928e5b8785ebe045cecea51091b8632
Vitor Cyriaco <vitorcyriaco@outlook.com>
2025-03-25 21:57:50 -0300

    Feat: created first page and add icon

--- src/view/Home.java
+++ src/view/Home.java
@@ -1,12 +1,26 @@
 package view;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Home extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        Image imgIconStarbucks = new Image(getClass().getResourceAsStream("/view/img/iconSmall.png"));
+
+        BorderPane mainPane = new BorderPane();
+        mainPane.setStyle("-fx-background-image: url('/view/img/bg.jpg')");
+        Scene scene = new Scene(mainPane, 1024, 700);
+        stage.setScene(scene);
+        stage.setTitle("Starbucks");
+        stage.getIcons().add(imgIconStarbucks);
+        stage.setMaximized(true);
+        stage.show();
 
     }
 
